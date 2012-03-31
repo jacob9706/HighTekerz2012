7499ac307afcd97c42c33cfe1678af17e460e1b9
synthmusic <synthmusic@gmail.com>
2012-03-31 11:59:17 -0700

    fixed bad checkin

--- SubSystems/Shooter.h
+++ SubSystems/Shooter.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef __SHOOTER_H
 #define __SHOOTER_H
 
@@ -38,44 +37,3 @@ private:
 };
 
 #endif
-=======
-#ifndef __SHOOTER_H
-#define __SHOOTER_H
-
-class Victor;
-class Encoder;
-class Solenoid;
-
-class Shooter
-{
-public:
-	Shooter(Victor *topWheel,
-			Victor *bottomWheel,
-			Victor *rotationMotor,
-			Victor *pitchMotor,
-			Encoder *topWheelEncoder,
-			Encoder *bottomWheelEncoder,
-			Encoder *rotationEncoder,
-			Encoder *pitchEncoder,
-			Solenoid *kicker);
-	~Shooter();
-	void Periodic(bool shoot);
-	void ManualRotate(float axis);
-	void ManualPitch(float axis);
-	bool IsShooting;
-	int leftCount;
-	int rightCount;
-private:
-	Victor *_topWheel;
-	Victor *_bottomWheel;
-	Victor *_rotationMotor;
-	Victor *_pitchMotor;
-	Encoder *_topWheelEncoder;
-	Encoder *_bottomWheelEncoder;
-	Encoder *_rotationEncoder;
-	Encoder *_pitchEncoder;
-	Solenoid *_kicker;
-};
-
-#endif
->>>>>>> 5cf0ff622f058e49bd836e49cbd450d29583cd9d
--- Util/Switch.cpp
+++ Util/Switch.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "Switch.h"
 
 Switch::Switch()
@@ -21,27 +20,3 @@ bool Switch::State(bool inputMomentary)
 	inputMomentaryLastState = inputMomentary;
 	return switchState;
 }
-=======
-#include "Switch.h"
-
-Switch::Switch()
-{
-	inputMomentaryLastState = false;
-	switchState = false;
-}
-
-Switch::~Switch()
-{
-	
-}
-
-bool Switch::State(bool inputMomentary)
-{
-	if (inputMomentary == true && inputMomentaryLastState == false)
-	{
-		switchState = !switchState;
-	}
-	inputMomentaryLastState = inputMomentary;
-	return switchState;
-}
->>>>>>> 5cf0ff622f058e49bd836e49cbd450d29583cd9d
